197aa1216b03827d819222c341744ad74b3c4f4f
unknown <chamberbankes@gmail.com>
2024-08-09 11:40:56 -0400

    add signupjs

--- public/js/newData.js
+++ public/js/newData.js
@@ -4,7 +4,7 @@ const newFormHandler = async function (event) {
   const message = document.querySelector('textarea[name="data-message"]').value;
 
   // Reminder- We were able to look at out project-2-setup-guide/controllers/api/exampleDataRoutes.js file to determine what our route is for this request
-  await fetch(`/api/exampleData`, {
+  await fetch(`/api/posts`, {
     // Reminder- Method will change depending on what we are doing to our API
     method: "POST",
     // Reminder- We need to make sure we are sending the correct data to our API by stringifying the data we captured from the form on line 4
