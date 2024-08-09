224e718659d7a938d89abae0acbdc8c8a405bb17
hamid assaghli <hamid@hamids-MBP.lan>
2024-08-09 13:32:58 -0400

    added tag and posttag api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,13 @@ const router = require("express").Router();
 // Import all of the routes from /api/ here
 const userRoutes = require("./userRoutes");
 const postRoutes = require("./postRoutes");
+const tagRoutes = require("./tagRoutes");
+const postTagRoutes = require("./postTagRoutes");
 
 // Connect the routes to the router here
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
+router.use("/tags", tagRoutes);
+router.use("/posttags", postTagRoutes);
 
 module.exports = router;
