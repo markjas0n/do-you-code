caf2d422915748673ddb9c8ab9ed80943a75ef5d
akaufmanfrey <akaufmanfrey@gmail.com>
2024-08-13 11:24:53 -0400

    added code for seeding

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,12 @@
 // Import modules for database connection and models
 const sequelize = require("../config/connection"); // Database connection
-const { User } = require("../models"); // Importing the User model
+const { Post, User, Tag, PostTag } = require("../models"); // Importing the User model
 
 //JSON data that will be seeded into the User table
 const userData = require("./userData.json");
-
+const postData = require('./postData.json');
+const tagData = require('./tagData.json');
+const postTagData = require('./postTagData.json');
 // Seeds the database with initial data
 const seedDatabase = async () => {
   // Sync all models with the database, force: true drops the tables first if they exist
@@ -17,7 +19,19 @@ const seedDatabase = async () => {
     returning: true, // Returns all newly created user instances
   });
   console.log("Users created"); // Log that users have been successfully created
-
+  await Post.bulkCreate(postData, {
+    individualHooks: true, // Ensures that beforeCreate and beforeUpdate hooks are run
+    returning: true, // Returns all newly created user instances
+  });
+  await Tag.bulkCreate(tagData, {
+    individualHooks: true, // Ensures that beforeCreate and beforeUpdate hooks are run
+    returning: true, // Returns all newly created user instances
+  });
+  console.log(postTagData);
+  await PostTag.bulkCreate(postTagData, {
+    individualHooks: true, // Ensures that beforeCreate and beforeUpdate hooks are run
+    returning: true, // Returns all newly created user instances
+  });
   // Exit the process after seeding is complete
   process.exit(0);
 };
